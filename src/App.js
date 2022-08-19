e80729f24d1296c5f72e4b40e4270fcc34013cf6
duonglamh1999 <duonglamh1999@gmail.com>
2022-08-18 23:21:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
+import {GET} from "./GetData";
+const App = () =>{
 
-function App() {
+  async function getData (){
+    const {name} = await GET();
+    console.log(name)
+  }
+useEffect(()=> {
+getData()
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className ="App">
+
+  </div>
   );
 }
 
